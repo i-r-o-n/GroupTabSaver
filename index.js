25614718d7b5060e11b3d89dc67fde911697c638
Irr0n <graphics.iron@gmail.com>
2023-02-04 18:08:58 -0800

    method for creating and grouping a single url

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const QueryInWindow = {windowId: chrome.windows.WINDOW_ID_CURRENT};
 
 class Listeners {
   static init() {
-    chrome.tabGroups.onUpdated.addListener(Listeners.onTabGroupUpdated);
+    chrome.tabGroups.onUpdated.addListener(Listeners.onGroupUpdated);
   };
 
-  static onTabGroupUpdated() {
+  static onGroupUpdated() {
     // communicate group 
     console.log("a tab group was updated!")
   };
@@ -30,7 +30,7 @@ class Reader {
   };
 
   // get all tab groups in active window
-  static async getTabGroups() {
+  static async getGroups() {
     return await chrome.tabGroups.query(QueryInWindow);
   };
 
@@ -47,7 +47,7 @@ Reader.Tabs = class {
   
 Reader.Groups = class {
 
-  static getTabGroupIds() {
+  static getGroupIds() {
     chrome.tabGroups.query(QueryInWindow, id);
   };
 
@@ -74,18 +74,37 @@ class Creator {
     return tabIds;
   };
 
+  static async addTabIdsToGroup(tabIds) {
+    await chrome.tabs.group({tabIds: tabIds});
+  };
+  
+
   static async createGroupFromURLs(urlList, title) {
     // try to get rid of dummy variable
     let tabIds = [];
     // create new tabs and append all ids to list
-    await urlList.map(url => chrome.tabs.create({url:url}, 
-      function(tab) {tabIds.push(tab.id)} ));
+    await urlList.map(url => chrome.tabs.create({url:url, active: false}, 
+      function(tab) {tabIds.push(tab.id)} )).then(
+        chrome.tabs.group({tabIds: tabIds})
+      );
 
-    chrome.tabs.group({tabIds: tabIds}, 
-      function(group) {chrome.tabGroups.update({
-        groupId: group.id, collapsed: true, title: title
-      })} );
-  }
+    // chrome.tabs.group({tabIds: tabIds[0]}, 
+    //   function(group) {chrome.tabGroups.update({
+    //     groupId: group.id, collapsed: true, title: title
+    //   })} );
+  };
+
+  static async createGroupFromURLs2(urlList, title) {
+    let _tabIds = [];
+    let _groupId = 0;
+    //add the first tab to the group
+    chrome.tabs.create({url:urlList[0], active: false}, 
+      function(tab) {chrome.tabs.group({tabIds: tab.id},
+        function(groupId) {_groupId = groupId; chrome.tabGroups.update(groupId = _groupId, {collapsed: true, title: title})} )})
+    await urlList.slice(1).map(url => chrome.tabs.create({url:url, active:false},
+      function(tab) {_tabIds.push(tab.id)}))
+    await chrome.tabs.group({tabIds: +_tabIds, groupId: _groupId})
+  };
 
   // group button pressed when group already exists
   static resetGroup() {
@@ -95,22 +114,24 @@ class Creator {
 }
 
 
-
+// TODO: remove upon integration with live page
+// button on test page for debugging purposes only
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
 
-  console.log(await Reader.getTabs());
-  console.log(await Reader.getTabGroups());
-  console.log((await Reader.getTabs()).map(tab => tab.favIconUrl));
-  console.log(await Creator.createGroupFromURLs(
+  // console.log(await Reader.getTabs());
+  // console.log(await Reader.getTabGroups());
+  // console.log((await Reader.getTabs()).map(tab => tab.favIconUrl));
+  console.log(await Creator.createGroupFromURLs2(
     ["https://google.com/", "https://bing.com/", "https://duckduckgo.com/"], "search"));
-
+  // console.log(await Creator.addTabIdsToGroup(await Creator.createTabsFromURLs(
+  //   ["https://google.com/", "https://bing.com/", "https://duckduckgo.com/"])));
 });
 
 
+// activate listeners
 function init() {
   Listeners.init();
-  console.log("initialized listeners");
 }
 
 init();
