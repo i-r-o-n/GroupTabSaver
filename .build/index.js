ae52a5b604b8cf7d53dfe459048ea6825df8c82b
Irr0n <graphics.iron@gmail.com>
2023-02-04 16:04:42 -0800

    initial commit

---
+++ .build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+function getTabGroups() {
+  chrome.tabGroups.query();
+}
+;
+function parseTabTrunk(tab) {
+}
+;
+function main() {
+  console.log(getTabGroups());
+}
+main();
+//# sourceMappingURL=index.js.map
