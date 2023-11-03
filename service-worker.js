2da3f54a2c067b2c9756e3b70a1266dfed99aaba
Theo <theomhalpern@gmail.com>
2023-11-03 12:50:31 -0400

    added error cecking for sameMembers

--- service-worker.js
+++ service-worker.js
@@ -37,10 +37,16 @@ function resetLocalStorage() {
 // resetLocalStorage()
 //code for checking whether 2 arrays have the same contence
 function sameMembers(arr1, arr2) {
+  try {
     const set1 = new Set(arr1);
     const set2 = new Set(arr2);
     return arr1.every(item => set2.has(item)) &&
         arr2.every(item => set1.has(item))
+  } catch (error) {
+    console.log(error)
+    return false;
+  }
+
 }
 
 async function updateLocalStorageKey() {
